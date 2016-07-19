ffa449e52b1812207a24acde9a6ecefa66494b1c
Peter Wang <happypeter1983@gmail.com>
2016-07-19 09:08:57 +0800

    wip

---
+++ dev-env/src/components/Main.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    let styles = {
+      root: {
+        fontFamily: 'sans-serif'
+      },
+      header: {
+        backgroundColor: '#00bcd4',
+        height: '8.4rem',
+        width: '100%',
+        textAlign: 'center'
+      },
+      logo: {
+        fontWeight: '600',
+        fontSize: '3rem',
+        letterSpacing: '-1px',
+        lineHeight: '8.4rem',
+        color: '#fff'
+      }
+    };
+    return (
+      <div style={styles.root}>
+        <header style={styles.header}>
+          <div style={styles.logo}>Haoqicat</div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Main;
--- dev-env/src/index.js
+++ dev-env/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-render(<h1>redux tower demo</h1>, document.getElementById('root'));
+import Main from './components/Main'
+
+render(<Main />, document.getElementById('root'));
