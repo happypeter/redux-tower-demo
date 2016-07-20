c294bed6b49dcbe403d102702684df083a82699e
Peter Wang <happypeter1983@gmail.com>
2016-07-20 15:42:51 +0800

    15

---
+++ 15/store.js
@@ -0,0 +1,29 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore} from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import courses from './data/courses';
+
+const defaultState = {
+  courses: courses,
+  comments: comments
+};
+
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+const store = createStore(rootReducer, defaultState, enhancers);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+if(module.hot) {
+  module.hot.accept('./reducers/',() => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
+export default store;
