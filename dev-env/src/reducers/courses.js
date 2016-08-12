9033915125f9d6dd70cc083c008dbb6fcd6a0c90
Peter Wang <happypeter1983@gmail.com>
2016-08-12 11:06:56 +0800

    11-update-state done

--- dev-env/src/reducers/courses.js
+++ dev-env/src/reducers/courses.js
@@ -1,6 +1,15 @@
 function courses(state = [], action) {
-  console.log(state, action);
-  return state;
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      const i = action.index;
+      return [
+        ...state.slice(0,i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i+1)
+      ]
+    default:
+      return state;
+  }
 }
 
 export default courses;
