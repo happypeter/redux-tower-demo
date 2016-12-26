2396f51284ed903214b4a0e7e5a45dbc4f684bc6
Peter Wang <happypeter1983@gmail.com>
2016-12-26 15:41:35 +0800

    update image link

--- dev-env/src/data/courses.js
+++ dev-env/src/data/courses.js
@@ -3,37 +3,37 @@ const courses = [
       "id":"1",
       "likes":60,
       "name":"Meteor-React 小鸟课程",
-      "image":"http://o84cbt6xd.bkt.clouddn.com/meteor-react-bird.png"
+      "image":"http://o86bpj665.bkt.clouddn.com/posters/meteor-react-bird.png"
    },
    {
       "id":"2",
       "likes":80,
       "name":"Happypeter 的摩登 JS 王国",
-      "image":"http://o84cbt6xd.bkt.clouddn.com/happypeter-js-kingdom.png"
+      "image":"http://o86bpj665.bkt.clouddn.com/posters/meteor-react-bird.png"
    },
    {
       "id":"3",
       "likes":30,
       "name":"跟 Peter 学 HTTP",
-      "image":"http://o84cbt6xd.bkt.clouddn.com/http-with-peter.png"
+      "image":"http://o86bpj665.bkt.clouddn.com/posters/meteor-react-bird.png"
    },
    {
       "id":"4",
       "likes":50,
       "name":"Webpack-React 鼹鼠",
-      "image":"http://o84cbt6xd.bkt.clouddn.com/webpack-react-mole.png"
+      "image":"http://o86bpj665.bkt.clouddn.com/posters/meteor-react-bird.png"
    },
    {
       "id":"5",
       "likes":50,
       "name":"Gulp-Flexbox 响应式网站课程",
-      "image":"http://o84cbt6xd.bkt.clouddn.com/gulp-flex-res.png"
+      "image":"http://o86bpj665.bkt.clouddn.com/posters/meteor-react-bird.png"
    },
    {
       "id":"6",
       "likes":40,
       "name":"驾驭命令行怪兽",
-      "image":"http://o84cbt6xd.bkt.clouddn.com/ride-cli-monster.png"
+      "image":"http://o86bpj665.bkt.clouddn.com/posters/meteor-react-bird.png"
    }
 ];
 
